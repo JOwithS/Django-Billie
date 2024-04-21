4d7d48070baf7f649aeb85d06235a581bd001a40
JOwithS <jo.ureta@duocuc.cl>
2024-04-21 13:51:15 -0400

    comienzo del front

---
+++ motor.js
@@ -0,0 +1,6 @@
+// La función se ejecutará cuando se cargue la página con windows q es pestaña en ENG
+window.onload = function() {
+    // Ocultar el reproductor de audio al cargar la página
+    var audioElement = document.getElementById('backgroundAudio');
+    audioElement.style.display = 'none';
+};
